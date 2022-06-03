d6aaa1877aaf9cf432e3ad4d4b701fec1e3eb228
Binghan Lin <binghan.lin@shonCloud.com>
2022-06-03 10:36:42 +0800

    update operator unit test

--- CUR_GRID_FDM/tests/test_OperatorFDM3D.py
+++ CUR_GRID_FDM/tests/test_OperatorFDM3D.py
@@ -5,25 +5,73 @@ import numpy as np
 
 
 class OperatorFDM3DTestCase(unittest.TestCase):
-    # def setUp(self):
+    def setUp(self):
+        mesh = RectangularMesh(5.0, 6, 5.0, 11, 6.0, 4)
+        self.op = OperatorFDM3D(mesh)
     # def tearDown(self):
 
     def test_Der_1(self):
+
+        dense_i = self.op.der_1('i').todense()
+        dense_j = self.op.der_1('j').todense()
+        dense_k = self.op.der_1('k').todense()
+
+        self.assertTrue(dense_i[0, 0] == -1.5)
+        self.assertTrue(dense_i[0, 1] == 2.0)
+        self.assertTrue(dense_i[0, 2] == -0.5)
+
+        self.assertTrue(dense_i[1, 0] == -0.5)
+        self.assertTrue(dense_i[1, 1] == 0.0)
+        self.assertTrue(dense_i[1, 2] == 0.5)
+
+        self.assertTrue(dense_j[0, 0] == -1.5)
+        self.assertTrue(dense_j[0, 6] == 2.0)
+        self.assertTrue(dense_j[0, 12] == -0.5)
+
+        self.assertTrue(dense_k[0, 0] == -1.5)
+        self.assertTrue(dense_k[0, 66] == 2.0)
+        self.assertTrue(dense_k[0, 132] == -0.5)
+
+        self.assertTrue(self.op.der_1('i').shape[0]
+                        == self.op.der_1('i').shape[1] == 264)
+        self.assertTrue(self.op.der_1('j').shape[0]
+                        == self.op.der_1('j').shape[1] == 264)
+        self.assertTrue(self.op.der_1('k').shape[0]
+                        == self.op.der_1('k').shape[1] == 264)
+
+    def test_Der_2(self):
         mesh = RectangularMesh(5.0, 6, 5.0, 11, 6.0, 4)
         op = OperatorFDM3D(mesh)
 
-        self.assertTrue(np.all(op.der_1('i')*mesh.x_flatten() == 1.0))
-        self.assertTrue(np.all(op.der_1('j')*mesh.y_flatten() == 0.5))
-        self.assertTrue(np.all(op.der_1('k')*mesh.z_flatten() == 2.0))
+        dense_i = self.op.der_2('i').todense()
+        dense_j = self.op.der_2('j').todense()
+        dense_k = self.op.der_2('k').todense()
 
-    # def test_Der_2(self):
-    #     mesh = RectangularMesh(5.0, 6, 5.0, 6, 5.0, 6)
-    #     op = OperatorFDM3D(mesh)
+        self.assertTrue(dense_i[0, 0] == 2.0)
+        self.assertTrue(dense_i[0, 1] == -5.0)
+        self.assertTrue(dense_i[0, 2] == 4.0)
+        self.assertTrue(dense_i[0, 3] == -1.0)
 
-    #     print(op.der_2('i')*mesh.x_flatten())
-        # self.assertTrue(np.all(op.der_2('i')*mesh.x_flatten() == 1.0))
-        # self.assertTrue(np.all(op.der_1('j')*mesh.y_flatten() == 0.5))
-        # self.assertTrue(np.all(op.der_1('k')*mesh.z_flatten() == 2.0))
+        self.assertTrue(dense_i[1, 0] == 1.0)
+        self.assertTrue(dense_i[1, 1] == -2.0)
+        self.assertTrue(dense_i[1, 2] == 1.0)
+
+        self.assertTrue(dense_j[0, 0] == 2.0)
+        self.assertTrue(dense_j[0, 6] == -5.0)
+        self.assertTrue(dense_j[0, 12] == 4.0)
+        self.assertTrue(dense_j[0, 18] == -1.0)
+
+        self.assertTrue(dense_k[0, 0] == 2.0)
+        self.assertTrue(dense_k[0, 66] == -5.0)
+        self.assertTrue(dense_k[0, 132] == 4.0)
+        self.assertTrue(dense_k[0, 198] == -1.0)
+
+        self.assertTrue(self.op.der_2('i').shape[0]
+                        == self.op.der_2('i').shape[1] == 264)
+        self.assertTrue(self.op.der_2('j').shape[0]
+                        == self.op.der_2('j').shape[1] == 264)
+        self.assertTrue(self.op.der_2('k').shape[0]
+                        == self.op.der_2('k').shape[1] == 264)
 
 
 if __name__ == "__main__":
